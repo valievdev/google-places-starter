a74d987e79bb4f423c4045ee5bcac4ba60a7c1ad
azat valiev <azvaliev@gmail.com>
2022-05-09 19:30:16 -0700

    basic version complete

--- main.ts
+++ main.ts
@@ -9,33 +9,40 @@ const loader = new Loader('AIzaSyB958cLfVvFD0H3wCpzpUUTHKc7xm-S9xI', options);
 
 const google = await loader.load();
 
-const findPlaces = () => {
 
+
+const findPlaces = () => {
   const map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+    center: {lat: 47.603230, lng: -122.330280},
     zoom: 8,
   });
   
-  const request = {
-    query: 'Museum of Contemporary Art Australia',
-    fields: ['name', 'geometry'],
+  const request: google.maps.places.PlaceSearchRequest = {
+    keyword: document.querySelector("#search").value,
+    location: {lat: 47.603230, lng: -122.330280},
+    radius: 10000
   };
 
-  const service = new google.maps.places.PlacesService(map);
-
-  service.findPlaceFromQuery(request, (results, status) => {
+  const service = new google.maps.places.PlacesService(map)
+  service.nearbySearch(request, function(results, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
-      // loop through results
-      // create new class instance of new google.maps.Marker
-      // position: myLatLng, -> You can get from the results[i].geometry.location
-      // map, -> map is created above
-      // title: "Hello World!", -> Grab this title from the current resullts[i].
-      
-      // methods to call on map like map.whatever()
-      // center the map on one of the markers
-      // zoom in
+      console.log(results)
+      for (var i = 0; i < results.length; i++) {
+        new google.maps.Marker({
+          position: results[i].geometry.location,
+          map,
+          title: results[i].name
+        })
+      }
+      map.setCenter(results[0].geometry.location)
+      map.setZoom(12)
     }
   });
 
+
 }
-findPlaces();
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  findPlaces();
+})
